701e5890a1b1787f411e675b0ae5cd8cce2a5e30
ibraheemovicjnr4 <ibraheemariyo4190@gmail.com>
2025-08-08 08:22:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: '',
   author: '‎',
   botName: ".",
-  botOwner: '103', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'CHATGPT', // Your name
+  ownerNumber: '2349137139054', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
